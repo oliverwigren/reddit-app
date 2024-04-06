7cd79657880221821237fb0e924bc2ec4455b49f
Oliver Wigren <oliver.wigren@hotmail.com>
2024-04-06 10:59:57 +0200

    Header comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello, world</h1>
-      </header>
+      <Header>
+        
+      </Header>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      {/* <img src=""/> */}
+      <h1>Reddit Light</h1>
+    </div>
+  );
+}
+
+export default Header;
